13ab7d2b6531d0085fced21db5da9e29209c227b
Danilovdenis1105 <danilovdenis479@gmail.com>
2020-10-02 15:39:12 +0300

    edit

--- app/js/main.js
+++ app/js/main.js
@@ -12,5 +12,15 @@ $(function () {
 		arrows: false,
 		slidesToShow: 4,
 		slidesToScroll: 4,
-	})
+	});
+
+	$(".js-range-slider").ionRangeSlider({
+		type: "double",
+		min: 0,
+		max: 1000,
+		from: 200,
+		to: 500,
+		grid: true
+	});
+
 });
