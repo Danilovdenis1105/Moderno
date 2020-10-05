a5bbf645032458c48e194b44fcf98a130c4f3003
Danilovdenis1105 <danilovdenis479@gmail.com>
2020-10-05 15:21:04 +0300

    added new items page

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,10 @@
 $(function () {
-	var mixer = mixitup('.products__inner-box');
 
 	$(".rate-star").rateYo({
 		rating: 5,
 		starWidth: "12px",
-		readOnly: true
+		readOnly: true,
+		spacing: "2px"
 	});
 
 	$(".product-slider__inner").slick({
@@ -14,5 +14,24 @@ $(function () {
 		slidesToScroll: 4,
 	});
 
+	$(".js-range-slider").ionRangeSlider({
+		type: "double",
+		min: 0,
+		max: 1000,
+		from: 200,
+		to: 500,
+		grid: true
+	});
 
+	$(".icon-th-list").on('click', function () {
+		$('.product-page__items').addClass('list');
+		$('.icon-th-list').addClass('active');
+		$('.icon-th-large').removeClass('active');
+	});
+	$(".icon-th-large").on('click', function () {
+		$('.product-page__items').removeClass('list');
+		$('.icon-th-large').addClass('active');
+		$('.icon-th-list').removeClass('active');
+	});
+	var mixer = mixitup('.products__inner-box');
 });
